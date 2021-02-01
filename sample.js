dda1f4f896a6bd1789d3e12f7542f33085322ed3
Manasa <manasapeela24@outlook.com>
2021-02-01 17:15:34 +0530

    hhhj

---
+++ sample.js
@@ -0,0 +1,17 @@
+< !DOCTYPE html >
+    <
+    html >
+    <
+    body >
+    <
+    p > Click "Try it"
+to call afunction with arguments < /p>    <
+    button onclick = "myFunction('Harry Potter','Wizard')" > Try it < /button> <
+    p id = "demo" > < /p>     <
+    script >
+    function myFunction(name, job) {
+        document.getElementById("demo").innerHTML = "Welcome " + name + ", the " + job + ".";
+    } <
+    /script>      <
+    / body >  <
+    /html>
